f707d607eae3555da988a890d30c5242f6d29a4b
dhruvbhavsar72 <70563033+dhruvbhavsar72@users.noreply.github.com>
2022-02-23 17:33:06 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -1,165 +1,158 @@
-let slide = document.querySelector('.slider_wrapper');
-const slideItem = document.querySelectorAll('.slider__item');
-var sliderWidth = document.querySelector('.slide-container').clientWidth;
-const slidedots = document.querySelector('.slide__dots');
-
+var sliderWidth = $('.slider_wrapper').width();
 let timeout;
-let password = document.getElementById('password')
-let strengthBadge = document.getElementById('StrengthDisp');
-let textCount = document.getElementById("textCount");
-let counterText = document.getElementById('counterText');
-let inquireForm = document.querySelector('.inquire__frm');
-let confirmpassword = document.getElementsByName('cpwd')[0];
-
-var tabWidth = document.querySelector('.tab__content').clientWidth;
-const prevbtn = document.querySelector('.slideprev');
-const nextbtn = document.querySelector('.slidenext');
-
+let password = $('#password');
+var tabWidth = $('.tab__content').width();
 
 var slideCounter = 0;
 
-
-
 $(window).resize(() => {
-
-    sliderWidth = document.querySelector('.slide-container').clientWidth;
-
+    sliderWidth = $('.slider_wrapper').width();
 })
 
 //onclick
 slideChange = () => {
-    slide.style.transform = 'translateX(' + (-sliderWidth * slideCounter) + 'px)';
+    $('.slider_wrapper').css('transform', `translateX(${-sliderWidth * slideCounter}px)`);
 }
 
-for (let index = 0; index < slideItem.length; index++) {
+for (let index = 0; index < $('.slider__item').length; index++) {
     let html = `<button class="slide__dots-btn" aria-label="slide button ${index + 1}" data-slide='${index}'></button>`
-    slidedots.insertAdjacentHTML("beforeend", `${html}`);
+    $('.slide__dots').append(html);
 }
 
-let btndots = document.querySelectorAll('.slide__dots-btn');
-
-btndots.forEach((i) => {
-
-    i.addEventListener('click', function (e) {
-        let activebtn = document.querySelector('.slide__dots-btn.activeSlide');
-        activebtn ? activebtn.classList.remove('activeSlide') : e.currentTarget.classList.add('activeSlide');
-        slideCounter = +this.dataset.slide;
-        slideChange();
+$('.slide__dots-btn').each(function () {
+    $(this).on('click', function (e) {
+        if (!$(this).hasClass('activeSlide')) {
+            $(this).addClass('activeSlide');
+            $(this).siblings().removeClass('activeSlide');
+            slideCounter = $(this).data('slide');
+            slideChange();
+        }
     })
-});
+})
 
-let tablink = document.querySelectorAll('.tablink');
-
-tablink.forEach((i) => {
-
-    i.addEventListener('click', function (e) {
-        e.preventDefault()
-        let activebtn = document.querySelector('.tablink.activeTab');
-        activebtn.classList.remove('activeTab'); e.currentTarget.classList.add('activeTab');
-        document.querySelector('.tab__content-tab.activeTabContent').classList.remove('activeTabContent');
-        document.querySelector(e.currentTarget.hash).classList.add('activeTabContent');
+$('.tablink').each(function () {
+    $(this).on('click', function (e) {
+        e.preventDefault();
+        if (!$(this).hasClass('activeTab')) {
+            $(this).addClass('activeTab');
+            $(this).parent('li').siblings().find('.activeTab').removeClass('activeTab');
+            let othertab = $(this).parent('li').siblings().find('a').attr('href');
+            let tabpane = $(this).attr('href')
+            $(tabpane).show();
+            $(othertab).hide();
+            $(tabpane).addClass('activeTabContent');
+            $(othertab).removeClass('activeTabContent');
+        }
         TabsliderChange()
-    })
+    });
 });
 
 //tabslider
-function TabsliderChange() {
-    const tabslide = document.querySelector('.tab__content-tab.activeTabContent .brand_slider');
-    const tabItem = document.querySelectorAll('.tab__content-tab.activeTabContent .brand__img-items');
+TabsliderChange = () => {
+    const tabslide = $('.tab__content-tab.activeTabContent .brand_slider');
+    const tabItem = $('.tab__content-tab.activeTabContent .brand__img-items');
     $(window).resize(() => {
-        tabWidth = document.querySelector('.tab__content').clientWidth
-    
+        tabWidth = $('.tab__content').width();
     })
     let tabCounter = 0;
-
+    let mql = window.matchMedia('(min-width: 992px)')
     tabChange = () => {
-        tabslide.style.transform = 'translateX(' + ((-tabWidth * tabCounter) / 5) + 'px)';
+        tabslide.css('transform', `translateX(${(-tabWidth * tabCounter) / (mql.matches ? 5 : 3)}px`)
     }
-    nextbtn.addEventListener('click', () => {
+    $('.slidenext').on('click', () => {
         if (tabCounter === tabItem.length - 1) {
             tabCounter = 0;
         }
         else {
-            
+
             tabCounter++;
         }
         tabChange();
     });
 
-    prevbtn.addEventListener('click', () => {
+    $('.slideprev').on('click', () => {
         if (tabCounter === 0) {
-            
+
             tabCounter = tabItem.length - 1;
         }
         else {
             tabCounter--;
         }
-
         tabChange();
     });
 }
 
 TabsliderChange();
-
-document.querySelector('.mobile-trigger').addEventListener('click', function () {
-    this.classList.toggle('active-re');
-    document.querySelector('.header__nav--lists').classList.toggle('mobileMenuActive')
+$('.mobile-trigger').on('click', () => {
+    $(this).toggleClass('active-re');
+    $('.header__nav--lists').toggleClass('mobileMenuActive')
 });
 
 
 let strongPassword = /[!~!@#$%^&_]/m;
-function StrengthChecker() {
-
+var passval;
+StrengthChecker = () => {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-        if (password.value.match(strongPassword) && password.value.length > 8) {
-            strengthBadge.textContent = 'Strong';
-            $('.popup__message--fail').animate({ right: -100 + '%' }, 0.8);
-        }
-        else if (password.value.length === 0) {
-            strengthBadge.textContent = '8 digit password with must use of some special characters such as ~!@#$%^&*_';
-            $('.popup__message--fail').animate({ right: -100 + '%' }, 0.8);
+        if (passval.length > 8 && passval.match(strongPassword)) {
+            $('.popup__message--fail').removeClass('msgpopupActive');
+        } else if (passval.length === 0) {
+            $('#StrengthDisp').text('8 digit password with must use of some special characters such as ~!@#$%^&*_');
+            $('.popup__message--fail').removeClass('msgpopupActive');
         } else {
-            $('.popup__message--fail p').text(`You must enter with these character ~!@#$%^&*_`);
-            $('.popup__message--fail').animate({ right: 0 }, 0.8);
-            //strengthBadge.textContent = '8 digit password with must use of some special characters such as ~!@#$%^&*_'
+            $('.popup__message--fail p').text(`You must enter with these character ~!@#$%^&*_ with minimum 8 character`);
+            $('.popup__message--fail').addClass('msgpopupActive');
         }
-    }, 500);
+    }, 100);
 
 }
 
-password.addEventListener("input", () => { StrengthChecker() });
+password.on("input", () => { passval = password.val(); StrengthChecker() });
 
-
-
-confirmpassword.addEventListener("input", () => {
-    if (password.value === confirmpassword.value) {
-        $('input[type="submit"]').removeAttr('disabled');
-        $('.popup__message--fail').animate({ right: -100 + '%' }, 0.8);
-    }
-    else {
-        $('.popup__message--fail p').text(`Password Matches not`);
-        $('.popup__message--fail').animate({ right: 0 }, 0.8);
+$('[name="cpwd"]').on("input", () => {
+    if (passval.length > 8) {
+        if (passval === $('[name="cpwd"]').val()) {
+            $('input[type="submit"]').removeAttr('disabled');
+            $('.popup__message--fail').removeClass('msgpopupActive');
+        }
+        else {
+            $('.popup__message--fail p').text(`Password Matches not`);
+            $('.popup__message--fail').addClass('msgpopupActive');
+            $('input[type="submit"]').attr('disabled', true);
+        }
     }
 });
 
-textCount.addEventListener("input", () => {
+
+
+$('[name="email"]').on("input", () => {
+    let emailvalue = this.value.toLowerCase();
+    if (!emailvalue.match(/(\<|^)[\w\d._%+-]+@(?:[\w\d-]+\.)+(\w{2,})(\>|$)/i)) {
+        $('.popup__message--fail p').text(`email address not valid`);
+        $('.popup__message--fail').addClass('msgpopupActive');
+    }
+    else {
+        $('.popup__message--fail').removeClass('msgpopupActive');
+    }
+});
+
+$("#textCount").on("input", () => {
     var maxLength = 200;
-    var strLength = textCount.value.length;
+    var strLength = $("#textCount").val().length;
     var charRemain = (maxLength - strLength);
-    counterText.innerHTML = `${charRemain === 0 ? maxLength : charRemain} / ${maxLength}  (Run time character count)`;
+    $("#counterText").html(`${charRemain === 0 ? maxLength : charRemain} / ${maxLength}  (Run time character count)`);
 })
 
-inquireForm.addEventListener("submit", function (e) {
+$('.inquire__frm').on("submit", (e) => {
     e.preventDefault();
     $('.popup__message--success p').text(`Success Submit Message`);
-    $('.popup__message--success').animate({ right: 0 }, 800);
+    $('.popup__message--success').addClass('msgpopupActive');
     setTimeout(() => {
-        $('.popup__message--success').animate({ right: -100 + '%' }, 800);
+        $('.popup__message--success').removeClass('msgpopupActive');
     }, 3000);
 });
 
-$('.header__nav--listItem > a').click(function () {
+$('.header__nav--listItem > a').click(() => {
     $(this).next('ul').slideDown();
     $(this).parent().siblings().find('ul').slideUp();
 });
@@ -169,38 +162,24 @@ function changeTheme() {
     if (theme === 'dark') {
         $('html').addClass('dark');
     }
-    $('html.dark').css({ 'filter': 'invert(1)', 'background': '#000' });
-    $('html.dark img').css({ 'filter': 'invert(1)' });
 }
-
 changeTheme();
 $('.themechange').on('click', function () {
-
-    
-    if($('html').hasClass('dark')){
+    if ($('html').hasClass('dark')) {
         localStorage.setItem('setTheme', 'light');
         $('html').addClass('light');
         $('html').removeClass('dark');
-        $('html.light').css({ 'filter': 'unset', 'background': '#fff' });
-        $('html.light img').css({ 'filter': 'unset' });
-        $(this).html(`<svg viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-        width="24" height="24">
-        <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" stroke="#0ea5e9"></path>
-        <path stroke="#0ea5e9"
-            d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
-            class="stroke-sky-500"></path>
-    </svg>`)
+        $(this).find('svg.light').removeClass('d-none').addClass('d-block');
+        $(this).find('svg.dark').addClass('d-none').removeClass('d-block')
     }
-    else{
+    else {
         localStorage.setItem('setTheme', 'dark');
         changeTheme();
-        $(this).html(`<svg viewBox="0 0 24 24" fill="none" width="24" height="24">
-    <path fill="#38bdf833" fill-rule="evenodd" clip-rule="evenodd" d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z" class="fill-sky-400/20"></path>
-    <path fill="#0ea5e9" d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z" class="fill-sky-500"></path>
-    <path fill="#0ea5e9" fill-rule="evenodd" clip-rule="evenodd" d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z" class="fill-sky-500"></path></svg>`)
+        $('html').removeClass('light');
+        $('html').addClass('dark');
+        $(this).find('svg.dark').removeClass('d-none').addClass('d-block')
+        $(this).find('svg.light').addClass('d-none').removeClass('d-block')
     }
-    
-    
 })
 
 
